9ad6ddcf4a8f90c2a60eb1739b9c8678637405af
林昱鋐 <zxcv96197@gmail.com>
2022-07-29 14:12:18 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link><br>
+    <router-link to="/about">About</router-link><br />
   </nav>
+  {{nameTest}}
   <router-view />
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      nameTest: process.env.VUE_APP_NAME
+    }
+  }
+}
+</script>
 <style lang="scss">
-  @import 'bootstrap'
+@import 'bootstrap';
 </style>
